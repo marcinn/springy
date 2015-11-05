ef3725bbc460fabfeb7a3e5258f76fc7034d1fac
Marcin Nowak <marcin.j.nowak@gmail.com>
2015-11-05 11:14:07 +0100

    add --no-confirm option for mgmt command

--- springy/management/commands/index.py
+++ springy/management/commands/index.py
@@ -14,11 +14,14 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('command', nargs=1, type=str)
         parser.add_argument('index', nargs='*', type=str)
+        parser.add_argument('--noconfirm', default=False,
+                action='store_true')
 
     def handle(self, **kw):
 
         command = kw['command'].pop()
         indices = kw['index']
+        no_confirm = kw['noconfirm']
 
         try:
             func = getattr(self, 'do_%s' % command)
@@ -35,24 +38,27 @@ class Command(BaseCommand):
         else:
             index_clasess = registry.get_all()
 
-        func(index_clasess)
+        func(index_clasess, no_confirm=no_confirm)
 
     def _call_indices(self, indices, method_name):
         for index_cls in indices:
             index = index_cls()
             getattr(index, method_name)()
 
-    def do_update(self, indices):
+    def do_update(self, indices, no_confirm=False):
         self._call_indices(indices, 'update_index')
 
-    def do_clear(self, indices):
-        if confirm('This will erase all documents from indexes: %s. Continue?' % indices):
+    def do_clear(self, indices, no_confirm=False):
+        indices_list = u'\n'.join(map(lambda x: u'\t- %s' % x, indices))
+        if no_confirm or confirm(
+                'This will erase all documents from indices:\n%s\n\nContinue?' % indices_list):
             self._call_indices(indices, 'clear_index')
 
-    def do_drop(self, indices):
-        if confirm('This command will drop indexes: %s. Continue?' % indices):
+    def do_drop(self, indices, no_confirm=False):
+        indices_list = u'\n'.join(map(lambda x: u'\t- %s' % x, indices))
+        if no_confirm or confirm('This command will drop indices:\n%s\n\nContinue?' % indices_list):
             self._call_indices(indices, 'drop_index')
 
-    def do_initialize(self, indices):
+    def do_initialize(self, indices, no_confirm=False):
         self._call_indices(indices, 'initialize')
 
