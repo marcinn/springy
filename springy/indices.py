0713a3fea0f5365926b524b47bb9271f5c4b565b
Marcin Nowak <marcin.j.nowak@gmail.com>
2016-07-01 16:19:23 +0200

    fix indices.py pep8, six related issue, and exc formatting error

--- springy/indices.py
+++ springy/indices.py
@@ -1,6 +1,5 @@
 from collections import defaultdict
 import itertools
-import six
 
 from .connections import get_connection_for_doctype
 from .fields import Field
@@ -28,7 +27,8 @@ class IndicesRegistry(object):
             raise ValueError('Index name can not be empty')
 
         if name in self._indices:
-            raise AlreadyRegisteredError('Index `%s` is already registered' % name)
+            raise AlreadyRegisteredError(
+                    'Index `%s` is already registered' % name)
 
         self._indices[name] = cls
 
@@ -48,7 +48,7 @@ class IndicesRegistry(object):
         return self._indices.values()
 
     def get_for_model(self, model):
-        return self._model_indices[model][:] # shallow copy
+        return self._model_indices[model][:]  # shallow copy
 
     def unregister(self, cls):
         to_unregister = []
@@ -64,7 +64,7 @@ class IndicesRegistry(object):
             del self._indices[name]
 
     def unregister_all(self):
-        self._indices={}
+        self._indices = {}
         self._model_indices = defaultdict(list)
 
 
@@ -82,10 +82,10 @@ class IndexOptions(object):
         self._declared_fields = declared_fields
 
     def setup_doctype(self, meta, index):
-        self.document = model_doctype_factory(meta.model, index,
-            fields=getattr(meta, 'fields', None),
-            exclude=getattr(meta, 'exclude', None)
-        )
+        self.document = model_doctype_factory(
+                meta.model, index,
+                fields=getattr(meta, 'fields', None),
+                exclude=getattr(meta, 'exclude', None))
 
 
 class IndexBase(type):
@@ -105,7 +105,7 @@ class IndexBase(type):
         declared_fields = {}
         for _attrname, _attr in new_class.__dict__.items():
             if isinstance(_attr, Field):
-                declared_fields[_attrname]=_attr
+                declared_fields[_attrname] = _attr
 
         setattr(new_class, '_meta', IndexOptions(meta, declared_fields))
         setattr(new_class, 'model', getattr(meta, 'model', None))
@@ -113,12 +113,14 @@ class IndexBase(type):
         if not new_class._meta.document:
             new_class._meta.setup_doctype(meta, new_class)
 
-        setattr(new_class, '_schema', Schema(new_class._meta.document.get_all_fields()))
+        setattr(new_class, '_schema', Schema(
+            new_class._meta.document.get_all_fields()))
 
         schema_fields = new_class._schema.get_field_names()
         for fieldname in new_class._meta._field_names:
-            if not fieldname in schema_fields:
-                raise FieldDoesNotExist('Field `%s` is not defined')
+            if fieldname not in schema_fields:
+                raise FieldDoesNotExist(
+                        'Field `%s` is not defined' % fieldname)
 
         index_name = new_class._meta.index or generate_index_name(new_class)
         registry.register(index_name, new_class)
@@ -126,7 +128,9 @@ class IndexBase(type):
         return new_class
 
 
-class Index(six.with_metaclass(IndexBase)):
+class Index(object):
+    __metaclass__ = IndexBase
+
     @property
     def name(self):
         return self._meta.index
@@ -167,7 +171,8 @@ class Index(six.with_metaclass(IndexBase)):
     def query_string(self, query):
         """
         Query index with `query_string` and EDisMax parser.
-        This is shortcut for `.query('query_string', query='<terms>', use_dis_max=True)`
+        This is shortcut for `.query('query_string', query='<terms>',
+            use_dis_max=True)`
         """
         return self.get_search_object().parse(query)
 
@@ -200,7 +205,8 @@ class Index(six.with_metaclass(IndexBase)):
         Delete document that represents specified `obj` instance.
 
         Raise DocumentDoesNotExist exception when document does not exist.
-        When `fail_silently` set to true, DocumentDoesNotExist will be silenced.
+        When `fail_silently` set to true, DocumentDoesNotExist will be
+        silenced.
         """
 
         from elasticsearch.exceptions import NotFoundError
@@ -210,8 +216,9 @@ class Index(six.with_metaclass(IndexBase)):
             doc.delete()
         except NotFoundError:
             if not fail_silently:
-                raise DocumentDoesNotExist('Document `%s` (id=%s) does not exists in index `%s`' % (
-                    doc._doc_type.name, doc.meta.id, self.name))
+                raise DocumentDoesNotExist(
+                    'Document `%s` (id=%s) does not exists in index `%s`' % (
+                        doc._doc_type.name, doc.meta.id, self.name))
 
     def save(self, obj, force=False):
         doc = self.to_doctype(obj)
@@ -228,19 +235,21 @@ class Index(six.with_metaclass(IndexBase)):
         doctype_name = self._meta.document._doc_type.name
         index_name = self._meta.document._doc_type.index
 
-        connection = get_connection_for_doctype(self._meta.document, using=using)
+        connection = get_connection_for_doctype(
+                self._meta.document, using=using)
 
         def document_to_action(x):
             data = x.to_dict()
             data['_op_type'] = 'index'
-            for key,val in x.meta.to_dict().items():
+            for key, val in x.meta.to_dict().items():
                 data['_%s' % key] = val
             return data
 
         actions = itertools.imap(document_to_action, generate_qs())
         consistency = consistency or self._meta.write_consistency
 
-        return bulk(connection, actions, index=index_name, doc_type=doctype_name,
+        return bulk(
+                connection, actions, index=index_name, doc_type=doctype_name,
                 consistency=consistency, refresh=True)[0]
 
     def update(self, obj):
@@ -264,12 +273,14 @@ class Index(six.with_metaclass(IndexBase)):
         return self.save_many(qs)
 
     def update_index(self, using=None, consistency=None):
-        self.save_many(self.get_query_set(), using=using,
+        self.save_many(
+                self.get_query_set(), using=using,
                 consistency=consistency)
 
     def clear_index(self, using=None, consistency=None):
         from elasticsearch.helpers import scan, bulk
-        connection = get_connection_for_doctype(self._meta.document, using=using)
+        connection = get_connection_for_doctype(
+                self._meta.document, using=using)
         objs = scan(connection, _source_include=['__non_existent_field__'])
         index_name = self._meta.document._doc_type.index
 
@@ -279,11 +290,12 @@ class Index(six.with_metaclass(IndexBase)):
 
         actions = itertools.imap(document_to_action, objs)
         consistency = consistency or self._meta.write_consistency
-        bulk(connection, actions, index=index_name, consistency=consistency,
+        bulk(
+                connection, actions, index=index_name, consistency=consistency,
                 refresh=True)
 
     def drop_index(self, using=None):
         from elasticsearch.client.indices import IndicesClient
-        connection = get_connection_for_doctype(self._meta.document, using=using)
+        connection = get_connection_for_doctype(
+                self._meta.document, using=using)
         return IndicesClient(connection).delete(self._meta.index)
-
